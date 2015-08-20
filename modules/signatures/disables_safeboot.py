80632d6a181760c825837d3b01d3f6213620dd86
kevross33 <kevross33@gmail.com>
2015-08-20 14:08:06 +0100

    Create file disables_safeboot.py
    A signature to detect the various modifications to safeboot

---
+++ modules/signatures/disables_safeboot.py
@@ -0,0 +1,21 @@
+from lib.cuckoo.common.abstracts import Signature
+
+class DisablesSafeBoot(Signature):
+    name = "disables_safe_boot"
+    description = "Attempts to disable SafeBoot"
+    severity = 3
+    categories = ["stealth"]
+    authors = ["Kevin Ross"]
+    minimum = "1.2"
+
+    def run(self):
+        keys = [
+            ".*\\\\SYSTEM\\\\(Wow6432Node\\\\)?CurrentControlSet\\\\Control\\\\SafeBoot\\\\.*",
+            ".*\\\\SYSTEM\\\\(Wow6432Node\\\\)?ControlSet001\\\\Control\\\\SafeBoot\\\\.*",
+            ".*\\\\SYSTEM\\\\(Wow6432Node\\\\)?ControlSet002\\\\Control\\\\SafeBoot\\\\.*"
+        ]
+        for check in keys:
+            if self.check_write_key(pattern=check, regex=True):
+                return True
+
+        return False
